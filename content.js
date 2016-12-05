dc0fbb5db9e9dc14985e49b9dab46e736a2888cb
Ben Whittle <benwhittle31@gmail.com>
2016-12-05 02:35:09 -0800

    got play, pause, and search working -- still working on back

--- content.js
+++ content.js
@@ -1,47 +1,66 @@
-function isPlaying () {
-  return $('#PauseButton').css('visibility') == 'visible';
-}
-
-function togglePlayback () {
-  $('#ScreenAVPlayer').click();
-}
-
-function play () {
-  if (!isPlaying()) {
-    togglePlayback();
-  }
-}
-
-function pause () {
-  if (isPlaying()) {
-    togglePlayback();
-  }
-}
-
-function back () {
-  if (isPlaying()) {
-    $('#Back30').click();
-  }
-}
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.message === 'execScript') {
     sendResponse({message: 'running script'});
 
-    if (annyang) {
+    var $ = jQuery;
+
+    $(document).ready(function () {
+      var iFrame = $('#playerContainer')[0];
+
+      if (!iFrame) {
+        console.log('iframe not found with id \'playerContainer\'. Returning.');
+        return;
+      }
+
+      if (!iFrame.contentWindow) {
+        console.log('iframe has no property \'contentWindow\'.', iFrame);
+        return;
+      }
+
+      var sendMessage = function (fn) {
+        if (fn.indexOf('(') == -1) {
+          fn += '()';
+        }
+
+        iFrame.contentWindow.postMessage(fn, '*');
+      };
+
+      var play = function () {
+        sendMessage('Play');
+      };
+
+      var pause = function () {
+        sendMessage('Pause');
+      };
+
+      var back = function () {
+      };
+
+      var search = function (_, query) {
+        console.log('Searching for ' + query + '...');
+        sendMessage('Search(["' + query.trim() + '"])');
+      };
+
       console.log('Voice Control Activated...');
+
+      annyang.addCallback('result', function(phrases) {
+        console.log(phrases);
+      });
 
       annyang.addCommands({
         'play': play,
         'pause': pause,
+        'paws': pause,
         'start': play,
         'stop': pause,
-        'back': back
+        'back': back,
+        'search': {
+          'regexp': /^(search for) (.*)/,
+          'callback': search
+        }
       });
 
       annyang.start({ autoRestart: true });
-    } else {
-      console.log('annyang not defined...');
-    }
+    });
   }
 });
